f33003b10c5ea5cc16b0a56334f8d92b2680f4b3
Zem <lourdrivera123@gmail.com>
2014-11-26 01:03:13 +0800

    configure environment, add way/generators and create users table

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'social_networking',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
